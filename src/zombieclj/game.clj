f88a9627b373902fba788552f8eed5be51fad5b1
Magnar Sveen <magnars@gmail.com>
2014-04-21 18:07:52 +0200

    Start revealing tiles

--- src/zombieclj/game.clj
+++ src/zombieclj/game.clj
@@ -14,7 +14,36 @@
   {:face face})
 
 (defn create-game []
-  {:tiles (->> tiles
-               (map ->tile)
-               shuffle)
+  {:tiles (->> tiles (mapv ->tile) shuffle)
    :sand (repeat 30 :remain)})
+
+(defn- can-reveal? [game]
+  (< (count (filter :revealed? (:tiles game))) 2))
+
+(defn- pairs [[face count]]
+  (when (= count 2) face))
+
+(defn- find-matched-face [tiles]
+  (->> tiles
+       (filter :revealed?)
+       (map :face)
+       frequencies
+       (keep pairs)
+       first))
+
+(defn- match-tile [face tile]
+  (if (= face (:face tile))
+    (assoc tile :matched? true)
+    tile))
+
+(defn- match-tiles [game]
+  (->> game
+       :tiles
+       (mapv (partial match-tile (find-matched-face (:tiles game))))
+       (assoc game :tiles)))
+
+(defn reveal-tile [idx game]
+  (if (can-reveal? game)
+    (-> (assoc-in game [:tiles idx :revealed?] true)
+        match-tiles)
+    game))
--- test/zombieclj/game_test.clj
+++ test/zombieclj/game_test.clj
@@ -20,3 +20,38 @@
 
 (fact "Creates game with sand"
       (-> (create-game) :sand frequencies) => {:remain 30})
+
+(def sample-game
+  {:tiles [{:face :gy} {:face :h1} {:face :h2} {:face :h4}
+           {:face :h3} {:face :fg} {:face :h5} {:face :zo}
+           {:face :h5} {:face :h2} {:face :h4} {:face :zo}
+           {:face :h1} {:face :fg} {:face :h3} {:face :zo}]
+   :sand (repeat 30 :remain)})
+
+(fact "You can reveal a tile"
+      (->> sample-game
+           (reveal-tile 1)
+           :tiles
+           (filter :revealed?))
+      => [{:face :h1, :revealed? true}])
+
+(fact "You can't reveal the third tile"
+      (->> sample-game
+           (reveal-tile 1)
+           (reveal-tile 2)
+           (reveal-tile 3)
+           :tiles
+           (filter :revealed?))
+      => [{:face :h1, :revealed? true}
+          {:face :h2, :revealed? true}])
+
+(fact "You can reveal two tiles"
+      (->> sample-game
+           (reveal-tile 1)
+           (reveal-tile 12)
+           :tiles
+           (filter :matched?))
+      => [{:face :h1, :revealed? true, :matched? true}
+          {:face :h1, :revealed? true, :matched? true}])
+
+
